bb1378d0683ce9c61589d4bd3518c9dc25741399
Lauri Makkonen <ag4039@student.jamk.fi>
2026-04-28 19:11:47 +0300

    page2 loads now only if username is correct

--- frontend/src/app/Pages/login/page1.page.ts
+++ frontend/src/app/Pages/login/page1.page.ts
@@ -1,28 +1,28 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, inject, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
 import {
   IonContent,
   IonHeader,
   IonTitle,
   IonToolbar,
-} from '@ionic/angular/standalone';
+} from "@ionic/angular/standalone";
 
-import { addIcons } from 'ionicons';
-import { logoGoogle, logInOutline } from 'ionicons/icons';
+import { addIcons } from "ionicons";
+import { logoGoogle, logInOutline } from "ionicons/icons";
 import {
   SocialAuthService,
   GoogleSigninButtonModule,
   SocialUser,
   SocialLoginModule,
-} from '@abacritt/angularx-social-login';
-import { AuthService } from '../../auth.service';
+} from "@abacritt/angularx-social-login";
+import { AuthService } from "../../auth.service";
 
 @Component({
-  selector: 'app-page1',
-  templateUrl: './page1.page.html',
-  styleUrls: ['./page1.page.scss'],
+  selector: "app-page1",
+  templateUrl: "./page1.page.html",
+  styleUrls: ["./page1.page.scss"],
   standalone: true,
   imports: [
     IonContent,
@@ -39,7 +39,7 @@ export class Page1Page implements OnInit {
   private socauthService = inject(SocialAuthService);
   private authService = inject(AuthService);
   user!: SocialUser;
-  error = '';
+  error = "";
 
   constructor(private router: Router) {
     addIcons({ logoGoogle, logInOutline });
@@ -52,11 +52,11 @@ export class Page1Page implements OnInit {
     this.socauthService.authState.subscribe((user) => {
       if (user) {
         this.user = user;
-        console.log('Successfully logged in via Google button', user);
+        console.log("Successfully logged in via Google button", user);
 
         /* Lähetetään glogin-metodilla Googlen idToken backendiin josta saadaan JWT
          Myös userin id annetaan authServicelle, jotta sitä voidaan verrata siellä
-         backendistä saatuun userin id:hen. 
+         backendistä saatuun userin id:hen.
         */
 
         if (this.user != null) {
@@ -65,13 +65,12 @@ export class Page1Page implements OnInit {
             .subscribe((result) => {
               if (result === true) {
                 // this.router.navigate(['/secret']);
+                this.router.navigateByUrl("/page2", { replaceUrl: true });
               } else {
-                this.error = 'Tunnus tai salasana väärä';
+                this.error = "Tunnus tai salasana väärä";
               }
             });
         }
-
-        this.router.navigateByUrl('/page2', { replaceUrl: true });
       }
     });
   }
