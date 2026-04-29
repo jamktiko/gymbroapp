2385d0056af9825781df8da1de181c4ae6af3e11
Jarius, Korpisaari <ag3328@student.jamk.fi>
2026-04-29 12:50:18 +0300

    merge hommia

--- frontend/src/app/Pages/trainings/page2.page.ts
+++ frontend/src/app/Pages/trainings/page2.page.ts
@@ -13,10 +13,32 @@ import {
   IonFab,
   IonFabButton,
   IonIcon,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonAccordionGroup,
+  IonAccordion
 } from '@ionic/angular/standalone';
 
 import { addIcons } from 'ionicons';
 import { add } from 'ionicons/icons';
+
+// Määritellään liikkeen rakenne
+interface Exercise {
+  name: string;
+  isSelected: boolean;
+  reps?: number;
+  sets?: number;
+  weight?: number;
+}
+
+// Määritellään treeniohjelman rakenne
+interface WorkoutProgram {
+  id: number;
+  name: string;
+  exercises: Exercise[];
+  date: string;
+}
 
 @Component({
   selector: 'app-page2',
@@ -35,21 +57,51 @@ import { add } from 'ionicons/icons';
     IonFab,
     IonFabButton,
     IonIcon,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonAccordionGroup,
+    IonAccordion
   ],
 })
 export class Page2Page implements OnInit {
-  /* public user = {
-    firstName: 'Päällikkö',
-    lastName: 'Gymbro',
-    id: '12345'
-  };
-  */
+  // Lista, johon Local Storagesta haetut ohjelmat tallennetaan
+  savedPrograms: WorkoutProgram[] = [];
+ 
 
   constructor(private router: Router) {
     addIcons({ add });
   }
 
   ngOnInit() {}
+
+
+  // Ionicin elinkaarimetodi: ajetaan joka kerta kun sivu ladataan näkyviin
+  ionViewWillEnter() {
+    this.loadPrograms();
+  }
+
+
+  // Haetaan tiedot selaimen muistista. // BACKEND-MUUTOS: Poista 'loadPrograms' sisältö ja korvaa se 
+  // --- POISTETTAVA OSA ALKAA ---
+  // // BACKEND-MUUTOS: Tämä koodi lähtee pois
+  // const data = localStorage.getItem('treeniohjelmat');
+  // if (data) {
+  //   this.savedPrograms = JSON.parse(data) as WorkoutProgram[];
+  // }
+  // --- POISTETTAVA OSA PÄÄTTYY ---
+
+  // BACKEND-MUUTOS: Tähän tilalle tulee kutsu:
+  // this.http.get<WorkoutProgram[]>('api/url/täällä').subscribe(res => {
+  //   this.savedPrograms = res;
+  // });
+  loadPrograms() {
+    const data = localStorage.getItem('treeniohjelmat');
+    if (data) {
+      this.savedPrograms = JSON.parse(data);
+    }
+  }
+
 
   // Tämä funktio ohjaa käyttäjän uudelle sivulle
   lisaaOhjelma() {
