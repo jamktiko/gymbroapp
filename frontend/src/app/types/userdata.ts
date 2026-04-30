9c797f9ebd3ec2ca6fc0c994924dff7274e894ba
Lauri Makkonen <ag4039@student.jamk.fi>
2026-04-30 14:08:02 +0300

    added types

---
+++ frontend/src/app/types/userdata.ts
@@ -0,0 +1,49 @@
+export interface Move {
+  _id: string;
+  name: string;
+  type: string;
+  muscleGroup: string;
+  isDefault: boolean;
+  createdBy: string | null;
+}
+
+export interface Set {
+  reps: number;
+  weight: number;
+}
+
+export interface Exercise {
+  move: Move;
+  sets: Set[];
+}
+
+export interface TrainingProgram {
+  _id: string;
+  name: string;
+  description: string;
+  exercises: Exercise[];
+  isDefault: boolean;
+  __v: number;
+}
+
+export interface TrainingSession {
+  _id: string;
+  datetime: string;
+  exercises: Exercise[];
+  breakTimeSeconds: number;
+}
+
+export interface UserData {
+  _id: string;
+  googleId: string;
+  name: string;
+  email: string;
+  level: number;
+  exp: number;
+  weightUnit: string;
+  trainingPrograms: TrainingProgram[];
+  trainingSessions: TrainingSession[];
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
