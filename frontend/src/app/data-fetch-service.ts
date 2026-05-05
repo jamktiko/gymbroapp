70e4092489e6e2bda73dcdd3554dc6047f676826
Lauri Makkonen <ag4039@student.jamk.fi>
2026-05-05 12:04:47 +0300

    created boilerplate for all methods to be implemented to DataFetchService

--- frontend/src/app/data-fetch-service.ts
+++ frontend/src/app/data-fetch-service.ts
@@ -42,7 +42,16 @@ export class DataFetchService {
   //   // }
   // }
 
-  // [GET] fetches all user data by id from database
+  //
+  //
+  //
+  //
+
+  // ------------------------------ USER METHODS: ------------------------------
+
+  /**
+   * [GET] Fetches all user data by id from database.
+   */
   getUserDataById(): Observable<UserData> {
     // fetch JWT token from sessionStorage
     let sessionData;
@@ -64,8 +73,10 @@ export class DataFetchService {
     );
   }
 
-  // [PATCH] modifies currently logged in user's data
-  updateUserDataById() {
+  /**
+   * [PATCH] Modifies currently logged in user's data.
+   */
+  updateUserDataById(): Observable<UserData> {
     // fetch JWT token from sessionStorage
     let sessionData;
 
@@ -85,5 +96,84 @@ export class DataFetchService {
       `${this.apiurlUsers}/${sessionData.googleId}`,
       {}, // Provide payload data instead of headers here
     );
+  }
+
+  // ------------------------------ MOVE METHODS: ------------------------------
+
+  /**
+   * [GET] Fetches all default moves + user's custom created moves from database.
+   */
+  getAllMoves() {
+    //
+  }
+
+  /**
+   * [GET] Fetches a move by id from database.
+   */
+  getMoveById() {
+    //
+  }
+
+  /**
+   * [POST] Creates a new move and adds it to the database.
+   */
+  createMove() {
+    //
+  }
+
+  // ------------------------- TRAININGPROGRAM METHODS: -------------------------
+
+  /**
+   * [GET] Fetches all trainingprograms for currently logged in user.
+   */
+  getAllPrograms() {
+    //
+  }
+
+  /**
+   * [GET] Fetches a trainingprogram by id for currently logged in user.
+   * - this method is used when starting a new trainingsession
+   */
+  getProgramById() {
+    //
+  }
+
+  /**
+   * [POST] Creates a new trainingprogram and adds it to the database.
+   */
+  createProgram() {
+    //
+  }
+
+  /**
+   * [DELETE] Deletes a trainingprogram.
+   */
+  deleteProgram() {
+    //
+  }
+
+  // ------------------------- TRAININGSESSION METHODS: -------------------------
+
+  /**
+   * [GET] Fetches all trainingsession for currently logged in user.
+   */
+  getAllSession() {
+    //
+  }
+
+  /**
+   * [GET] Fetches a trainingsession by id for currently logged in user.
+   * - this method is used if we implement a calendar view at some point
+   * - ex. if user clicks on a specific date you could see more info about the session
+   */
+  getSessionById() {
+    //
+  }
+
+  /**
+   * [POST] Creates a new trainingsession and adds it to the database.
+   */
+  createSession() {
+    //
   }
 }
