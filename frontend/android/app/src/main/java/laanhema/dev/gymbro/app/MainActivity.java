389ffcf66b76edb74d215ed25e3908f140d363ec
Netta Tavi <ag3461@student.jamk.fi>
2026-05-11 11:50:34 +0300

    dark mode
    Dark mode pakotettu xml:ssä ja mainactivity.java

--- frontend/android/app/src/main/java/laanhema/dev/gymbro/app/MainActivity.java
+++ frontend/android/app/src/main/java/laanhema/dev/gymbro/app/MainActivity.java
@@ -1,5 +1,17 @@
 package laanhema.dev.gymbro.app;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatDelegate;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+      @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        // Pakota dark mode
+        AppCompatDelegate.setDefaultNightMode(
+                AppCompatDelegate.MODE_NIGHT_YES
+        );
+
+        super.onCreate(savedInstanceState);
+    }
+}
