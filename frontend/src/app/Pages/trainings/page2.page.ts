dacea80cb046639ade9b3ef7068cd50a1416f75c
Lauri Makkonen <ag4039@student.jamk.fi>
2026-05-14 15:41:43 +0300

    refactor page2 layout and clean up unused code

--- frontend/src/app/Pages/trainings/page2.page.ts
+++ frontend/src/app/Pages/trainings/page2.page.ts
@@ -9,7 +9,13 @@ import { Router } from '@angular/router';
 import { XpService } from '../../xp.service';
 import { AccordionGroupCustomEvent, ItemReorderEventDetail } from '@ionic/core';
 import { addIcons } from 'ionicons';
-import { add, trashOutline, play, reorderTwoOutline, pencilOutline } from 'ionicons/icons';
+import {
+  add,
+  trashOutline,
+  play,
+  reorderTwoOutline,
+  pencilOutline,
+} from 'ionicons/icons';
 import { TrainingProgram, UserData } from '../../types/userdata';
 import { DataFetchService } from '../../data-fetch-service';
 import {
@@ -64,13 +70,6 @@ export class Page2Page implements OnInit {
   private alertCtrl = inject(AlertController);
   public xpService = inject(XpService);
   private dataFetchService = inject(DataFetchService);
-
-  get levelProgress(): number {
-    if (!this.userData) return 0;
-    const lvl = this.userData.level;
-    const total = (lvl + 1) * (lvl + 1) * 50 - lvl * lvl * 50;
-    return 1 - this.userData.xpToNextLevel / total;
-  }
 
   public usersTrainingPrograms: TrainingProgram[] = [];
   public userData!: UserData;
